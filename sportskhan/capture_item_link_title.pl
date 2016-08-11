84d523da52cd359fce88ea3f707b9329130ccf04
Youngil Cho <terzeron@gmail.com>
2016-08-11 21:37:29 +0900

    add a feed and modify the capturing script for it

--- sportskhan/capture_item_link_title.pl
+++ sportskhan/capture_item_link_title.pl
@@ -10,23 +10,23 @@ use Getopt::Std;
 
 sub print_usage
 {
-	print "usage:\t$PROGRAM_NAME [ -n <limit of recent feeds> ]\n";
-	print "\n";
-	exit(-1);
+    print "usage:\t$PROGRAM_NAME [ -n <limit of recent feeds> ]\n";
+    print "\n";
+    exit(-1);
 }
 
 
 sub main
 {
-	my $link = "";
-	my $title = "";
-	my $url_prefix = "";
-	my $cartoon_id = 0;
-	my $episode_id = 0;
-	my $episode_num = 0;
-	my $state = 0;
+    my $link = "";
+    my $title = "";
+    my $url_prefix = "";
+    my $cartoon_id = 0;
+    my $episode_id = 0;
+    my $episode_num = 0;
+    my $state = 0;
 
-	my %opts = ();                                                              
+    my %opts = ();                                                              
     my $opt_str = "n:";                                                         
     my $limit_feeds = 10000;                                                    
     our $opt_n;                                                                 
@@ -39,30 +39,30 @@ sub main
         }                                                                       
     }                                                                           
 
-	my $count = 0;
-	while (my $line = <STDIN>) {
-		if ($state == 0) {
-			if ($line =~ m!document\.location\.href\s*=\s*"([^"]+)"!) {
-				$url_prefix = $1;
-				$state = 1;
-			}
-		} elsif ($state == 1) {
-			while ($line =~ m!<option value='(\d+)\|(\d+)'(?: selected)?>(.+?(\([^\)]+\))?)</option>!g) {
-				$cartoon_id = $1;
-				$episode_id = $2;
-				$title = $3;
-				$episode_num = $4;
+    my $count = 0;
+    while (my $line = <STDIN>) {
+	if ($state == 0) {
+	    if ($line =~ m!document\.location\.href\s*=\s*"([^"]+)"!) {
+		$url_prefix = $1;
+		$state = 1;
+	    }
+	} elsif ($state == 1) {
+	    while ($line =~ m!<option value='(\d+)\|(\d+)(?:\|\d+)?'(?: selected)?>(.+?(\([^\)]+\))?)</option>!g) {
+		$cartoon_id = $1;
+		$episode_id = $2;
+		$title = $3;
+		$episode_num = $4;
 				#$title .= "(" . $episode_num . ")";
-				$link = "http://sports.khan.co.kr/" . $url_prefix . $cartoon_id . "&page=" . $episode_id;
-				$link =~ s!&amp;!&!g;
-				print "$link\t$title\n";
-				$count++;
-				if ($count >= $limit_feeds) {
-					last;
-				}
-			}
+		$link = "http://sports.khan.co.kr/" . $url_prefix . $cartoon_id . "&page=" . $episode_id;
+		$link =~ s!&amp;!&!g;
+		print "$link\t$title\n";
+		$count++;
+		if ($count >= $limit_feeds) {
+		    last;
 		}
+	    }
 	}
+    }
 }
 
 
