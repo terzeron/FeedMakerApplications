69f6d6b7a3af1b614ae6b2a5d6f06ae5f6f9676c
Youngil Cho <terzeron@gmail.com>
2016-10-24 18:36:16 +0900

    modify the capturing pattern

--- smilekhan/capture_item_link_title.pl
+++ smilekhan/capture_item_link_title.pl
@@ -10,43 +10,43 @@ use Getopt::Std;
 
 sub print_usage
 {
-	print "usage:\t$PROGRAM_NAME [ -n <limit of recent feeds> ]\n";
-	print "\n";
-	exit(-1);
+    print "usage:\t$PROGRAM_NAME [ -n <limit of recent feeds> ]\n";
+    print "\n";
+    exit(-1);
 }
 
 
 sub main
 {
-	my $link = "";
-	my $title = "";
+    my $link = "";
+    my $title = "";
 
-	my %opts = ();                                                              
-    my $opt_str = "n:";                                                         
-    my $limit_feeds = 10000;                                                    
-    our $opt_n;                                                                 
-    getopts($opt_str, \%opts);                                                  
-    if (defined $opts{"n"}) {                                                   
-        if ($opts{"n"} =~ m!^\s*\d+\s*$!) {                                     
-            $limit_feeds = $opts{"n"};                                          
-        } else {                                                                
-            print_usage();                                                      
-        }                                                                       
-    }                                                                           
+    my %opts = ();
+    my $opt_str = "n:";
+    my $limit_feeds = 10000;
+    our $opt_n;
+    getopts($opt_str, \%opts);
+    if (defined $opts{"n"}) {
+        if ($opts{"n"} =~ m!^\s*\d+\s*$!) {
+            $limit_feeds = $opts{"n"};
+        } else {
+            print_usage();
+        }
+    }
 
-	my $count = 0;
-	while (my $line = <STDIN>) {
-		while ($line =~ m!<li><a href="/(cartoon/index\.html\?category=\d+&artid=\d+)">(?:<b>)?([^<]+)(?:</b>)?</a></li>!) {
-			$link = "http://smile.khan.co.kr/" . $1;
-			$title = $2;
-			$link =~ s!&amp;!&!g;
-			print "$link\t$title\n";
-			$count++;
-			if ($count >= $limit_feeds) {
-				last;
-			}
-		}
+    my $count = 0;
+    while (my $line = <STDIN>) {
+	while ($line =~ m!<a href="/(cartoon/index\.html\?category=\d+&artid=\d+)">(?:<b>)?([^<]+)(?:</b>)?</a>!) {
+	    $link = "http://smile.khan.co.kr/" . $1;
+	    $title = $2;
+	    $link =~ s!&amp;!&!g;
+	    print "$link\t$title\n";
+	    $count++;
+	    if ($count >= $limit_feeds) {
+		last;
+	    }
 	}
+    }
 }
 
 
