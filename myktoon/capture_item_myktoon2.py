db0684ddc3ed6f0878e61656d852e1c8cbfc5729
Youngil Cho <terzeron@gmail.com>
2016-10-24 18:24:13 +0900

    convert the perl scripts to python scripts add another collecting script

---
+++ myktoon/capture_item_myktoon2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import io
+import os
+import sys
+import re
+import getopt
+import collections
+import feedmakerutil
+
+
+def main():
+    link_prefix = "http://www.myktoon.com/web/times_view.kt?"
+    link = ""
+    title = ""
+    
+    optlist, args = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            numOfRecentFeeds = int(a)
+        
+    circularQueue = collections.deque([], maxlen=numOfRecentFeeds)
+    lineList = feedmakerutil.readStdinAsLineList()
+    state = 0
+    for line in lineList:
+        if state == 0:
+            m1 = re.search(r'<a href="/web/times_view.kt[^"]*webtoonseq=(?P<webtoonseq>\d+)&(amp;)?timesseq=(?P<timesseq>\d+)">', line)
+            if m1:
+                webtoonseq = m1.group("webtoonseq")
+                timesseq = m1.group("timesseq")
+                link = link_prefix + "webtoonseq=" + webtoonseq + "&timesseq=" + timesseq
+                state = 1
+        elif state == 1:
+            m2 = re.search(r'<span class="bkTitle">(?P<title>[^<]+)</span>', line)
+            if m2:
+                title = m2.group("title")
+                circularQueue.append((link, title))
+                state = 0
+
+    for (link, title) in circularQueue:
+        print("%s\t%s" % (link, title))
+            
+if __name__ == "__main__":
+	main()
+
---
+++ myktoon/post_process_myktoon.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import io
+import os
+import sys
+import re
+import getopt
+from urllib.parse import urlparse, parse_qs
+import requests
+import feedmakerutil
+
+
+def main():
+    # url parsing
+    url = sys.argv[1]
+    parsedUrl = urlparse(url)
+    parsedParams = parse_qs(parsedUrl.query)
+    webtoonseq = parsedParams['webtoonseq'][0]
+    timesseq = parsedParams['timesseq'][0]
+    dataUrl = "http://www.myktoon.com/api/work/getTimesDetailImageList.kt?mobileyn=N&webtoonseq=" + webtoonseq + "&timesseq=" + timesseq
+
+    # request
+    headers = { "Referer": url }
+    response = requests.get(dataUrl, headers=headers)
+    
+    # read data from reponse
+    for item in response.json()['imageList']:
+        print("<img src='%s' width='100%%'>" % item['imagepath'])
+
+        
+if __name__ == "__main__":
+    main()
