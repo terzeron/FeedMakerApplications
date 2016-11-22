b698963561ea7347d4c92ebefc2c39cd9dfd97f0
Youngil Cho <terzeron@gmail.com>
2016-11-22 15:00:06 +0900

    change a perl script for post processing to a python script

---
+++ moneytoday/post_process_moneytoday.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import feedmakerutil
+
+
+def main():
+    secondPageUrl = ""
+    imgUrlList = []
+
+    for line in feedmakerutil.readStdinAsLineList():
+        line = line.rstrip()
+        m = re.search(r"<img src='(?P<imgUrl>http://comicmenu.mt.co.kr/[^']+.jpg)'(?: width='\d+%')?/>", line)
+        if m:
+            imgUrl = m.group("imgUrl")
+            # 광고 이미지 skip
+            if re.search(r"http://comicmenu.mt.co.kr/banner/comic_\d+_100811.jpg", imgUrl):
+                continue
+            imgUrlList.append(imgUrl)
+
+        m = re.search(r"<a href='(?P<secondPageUrl>http://[^']+)'[^>]*>", line)
+        if m:
+            secondPageUrl = m.group("secondPageUrl")
+        else:
+            m = re.search(r"<img src='http://comicmenu\.mt\.co\.kr/images/btn_cartoon_2p\.gif'/>", line)
+            if m:
+                break
+
+    # 마지막 광고 이미지 제거
+    imgUrlList.pop()
+            
+    if secondPageUrl != "":
+        cmd = "wget.sh '%s' | extract.py '%s'" % (secondPageUrl, sys.argv[1])
+        result = feedmakerutil.execCmd(cmd)
+        for line in result.split("\n"):
+            m = re.search(r"<img src='(?P<imgUrl>http://comicmenu.mt.co.kr/[^']+.jpg)'(?: width='\d+%')?/>", line)
+            if m:
+                imgUrl = m.group("imgUrl")
+                if re.search(r"http://comicmenu.mt.co.kr/banner/comic_\d+_100811.jpg", imgUrl):
+                    continue
+                imgUrlList.append(imgUrl)
+
+    for imgUrl in imgUrlList:
+        print("<img src='%s' width='100%%'/>" % (imgUrl))
+
+        
+if __name__ == "__main__":
+    sys.exit(main())
