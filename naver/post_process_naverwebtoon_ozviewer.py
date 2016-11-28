62e7a48c3a4a55bb2c70b4bcc0b72af40c4a78a3
Youngil Cho <terzeron@gmail.com>
2016-11-28 11:33:44 +0900

    change a perl script for ozviewer processing to a python script

---
+++ naver/post_process_naverwebtoon_ozviewer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+
+import sys
+import os
+import re
+import subprocess
+import feedmakerutil
+from feedmakerutil import die, err, warn
+
+
+def main():
+    pageUrl = sys.argv[1]
+    urlPrefix = ""
+    dataUrl = ""
+
+    for line in feedmakerutil.readStdinAsLineList():
+        m = re.search(r"jpg: '(?P<urlPrefix>[^']+\/){=filename}\?type=[^']+'", line)
+        if m:
+            urlPrefix = m.group("urlPrefix")
+        else:
+            m = re.search(r"documentURL:\s*'(?P<dataUrl>[^']+)'", line)
+            if m:
+                dataUrl = m.group("dataUrl")
+                break
+      
+    if not dataUrl or urlPrefix:
+        die("can't get a data url from input")
+    
+    cmd = "wget.sh '%s' utf8 | gunzip 2> /dev/null || wget.sh '%s' utf8" % (dataUrl, dataUrl)
+    #print(cmd);
+    with subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE) as p:
+        for line in p.stdout:
+            line = line.rstrip()
+            matches = re.findall(r"[^\"]+\"\s*:\s*\"(assets/still/[^\"]+\.(?:png|jpg))\"", line)
+            for match in matches:
+                imgUrl = urlPrefix + match[1]
+                print("<img src='%s' width='100%'/>" % (imgUrl))
+
+                     
+if __name__ == "__main__":
+    sys.exit(main())
