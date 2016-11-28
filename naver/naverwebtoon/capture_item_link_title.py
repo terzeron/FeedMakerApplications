5fe3f06e9b787fd7e92f519ad3c940ce90b87f0a
Youngil Cho <terzeron@gmail.com>
2016-11-28 18:21:08 +0900

    change a perl script for collecting to a python script

---
+++ naver/naverwebtoon/capture_item_link_title.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+import sys
+import re
+import feedmakerutil
+
+
+def main():
+    link = ""
+    title = ""
+    state = 0
+    urlPrefix = "http://comic.naver.com"
+
+    for line in feedmakerutil.readStdinAsLineList():
+        m = re.search(r'<a href="(?P<url>/webtoon/list.nhn\?titleId=\d+)[^"]*"[^>]*><img[^>]*title="(?P<title>[^"]+)"[^>]*/?>', line)
+        if m:
+            url = m.group("url")
+            title = m.group("title")
+            link = urlPrefix + url
+            print("%s\t%s" % (link, title))
+
+            
+if __name__ == "__main__":
+    sys.exit(main())
