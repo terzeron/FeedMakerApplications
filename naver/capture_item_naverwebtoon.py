4d8ee01c13b3441a251603c0d8cc1237b4413713
Youngil Cho <terzeron@gmail.com>
2016-11-28 12:38:16 +0900

    modify a perl script for collecting naverwebtoon to a python script and modify some feeds according to the change

---
+++ naver/capture_item_naverwebtoon.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+
+import sys
+import re
+import feedmakerutil
+
+
+def main():
+    link = ""
+    title = ""
+    urlPrefix = "http://comic.naver.com/"
+
+    for line in feedmakerutil.readStdinAsLineList():
+        m = re.search(r'''
+        <a
+        \s+
+        href="/
+        (?P<url>
+        (?:
+        webtoon
+        |
+        challenge
+        |
+        bestChallenge
+        )
+        /detail\.nhn
+        [^"]*
+        no=(?P<title1>\d+)
+        [^"]*
+        )
+        "
+        [^>]*
+        >
+        (?P<title2>.+)
+        </a>
+        ''', line, re.VERBOSE)
+        if m:
+            url = m.group("url")
+            url = re.sub("&amp", "&", url)
+            url = re.sub(r"&week(day)?=\w\w\w", "", url)
+            link = urlPrefix + url
+            title = "%04d. %s" % (int(m.group("title1")), m.group("title2"))
+            print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
