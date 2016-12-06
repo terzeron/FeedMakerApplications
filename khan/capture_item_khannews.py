ed0980726de57d97aa37edcef459639fda0b0415
Youngil Cho <terzeron@gmail.com>
2016-12-06 20:50:23 +0900

    add a feed

---
+++ khan/capture_item_khannews.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import re
+import getopt
+import feedmakerutil
+
+
+def main():
+    link_prefix = "http://news.khan.co.kr/kh_news/"
+    
+    numOfRecentFeeds = 10
+    optlist, args = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            numOfRecentFeeds = int(a)
+    
+    lineList = feedmakerutil.readStdinAsLineList()
+    resultList = []
+    for line in lineList:
+        m1 = re.search(r'<a href="http://news.khan.co.kr/kh_news(?P<url>/khan_art_view.html[^"]+)"[^>]*>(\[[^\]]+\] )?(?P<title>[^<]+)</a>', line)
+        if m1:
+            link = link_prefix + m1.group("url")
+            link = re.sub(r'&amp;', '&', link)
+            title = m1.group("title")
+            resultList.append((link, title))
+
+    for (link, title) in resultList[:numOfRecentFeeds]:
+        print("%s\t%s" % (link, title))
+
+if __name__ == "__main__":
+    main()
