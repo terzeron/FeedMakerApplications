ff3264015e51141c4b385602a367b95b54a3bccc
Youngil Cho <terzeron@gmail.com>
2017-09-13 12:21:59 +0900

    modify the capturing pattern modify the coding style

--- tistory/capture_item_tistory.py
+++ tistory/capture_item_tistory.py
@@ -13,35 +13,50 @@ import feedmakerutil
 def main():
     link = ""
     title = ""
-    urlPrefix = ""
-    state = 0
+    url_prefix = ""
     
-    numOfRecentFeeds = 30
+    num_of_recent_feeds = 30
     optlist, args = getopt.getopt(sys.argv[1:], "n:")
     for o, a in optlist:
         if o == '-n':
-            numOfRecentFeeds = int(a)
+            num_of_recent_feeds = int(a)
 
-    resultList = []
-    for line in feedmakerutil.readStdinAsLineList():
+    result_list = []
+    list = feedmakerutil.readStdinAsLineList()
+    state = 0
+    for line in list:
         if state == 0:
             m = re.search(r'href="(?P<url_prefix>http://[^"/]+)"', line)
             if m:
-                urlPrefix = m.group("url_prefix")
+                url_prefix = m.group("url_prefix")
                 state = 1
         elif state == 1:
             m = re.search(r'<a href="(?P<article_id>/\d+)" class="link_post">', line)
             if m:
-                link = urlPrefix + m.group("article_id")
+                link = url_prefix + m.group("article_id")
                 state = 2
         elif state == 2:
             m = re.search(r'<strong class="tit_post">(?P<title>.+)</strong>', line)
             if m:
                 title = m.group("title")
-                resultList.append((link, title))
+                result_list.append((link, title))
                 state = 1
 
-    for (link, title) in resultList[:numOfRecentFeeds]:
+    state = 0
+    for line in list:
+        if state == 0:
+            m = re.search(r'url: "(?P<url_prefix>http://[^"]+)"', line)
+            if m:
+                url_prefix = m.group("url_prefix")
+                state = 1
+        elif state == 1:
+            m = re.search(r'<h2 class="title"><a href="(?P<link>[^"]+)">(?P<title>.*)</a>', line)
+            if m:
+                link = url_prefix + m.group("link")
+                title = m.group("title")
+                result_list.append((link, title))
+        
+    for (link, title) in result_list[:num_of_recent_feeds]:
         print("%s\t%s" % (link, title))
 
             
