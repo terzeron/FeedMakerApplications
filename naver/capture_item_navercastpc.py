fac1769a4e060d885005031257b363520e4793b1
Youngil Cho <terzeron@gmail.com>
2017-09-13 12:15:11 +0900

    modify the capturing pattern

--- naver/capture_item_navercastpc.py
+++ naver/capture_item_navercastpc.py
@@ -10,15 +10,22 @@ def main():
     link = ""
     urlPrefix = "http://terms.naver.com/"
     title = ""
+    state = 0
 
     for line in feedmakerutil.readStdinAsLineList():
-        m = re.search (r'<dt><a href="/(?P<url>entry\.nhn\?[^"]+)"><strong>(?P<title>[^<]+)</strong></a>', line)
-        if m:
-            url = m.group("url")
-            url = re.sub(r'&amp;', '&', url)
-            title = m.group("title")
-            link = urlPrefix + url
-            print("%s\t%s" % (link, title))
+        if state == 0:
+            m = re.search(r'<strong class="title">', line)
+            if m:
+                state = 1
+        elif state == 1:
+            m = re.search(r'<a href="/(?P<url>entry\.nhn\?[^"]+)">(?P<title>[^<]+)</a>', line)
+            if m:
+                url = m.group("url")
+                url = re.sub(r'&amp;', '&', url)
+                title = m.group("title")
+                link = urlPrefix + url
+                print("%s\t%s" % (link, title))
+                state = 0
 
             
 if __name__ == "__main__":
