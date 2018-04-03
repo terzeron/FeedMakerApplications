ed1c1d3ba5265d785894ae570c44279db5eb3aac
Youngil Cho <terzeron@gmail.com>
2018-04-03 16:50:20 +0900

    add new feeds

---
+++ marumaru/capture_item_marumaru.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+
+import io
+import os
+import sys
+import re
+import getopt
+from feedmakerutil import IO
+
+
+def main():
+    link = ""
+    title = ""
+
+    numOfRecentFeeds = 1000
+    optlist, args = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            numOfRecentFeeds = int(a)
+
+    lineList = IO.read_stdin_as_line_list()
+    resultList = []
+    for line in lineList:
+        matches = re.findall(r'<a href="(?P<link>[^"]+)"[^>]*target="_self">(?:<span[^>]*>)?(?P<title>.*?)(?:</span>)?</a>', line)
+        for m in matches:
+            link = m[0]
+            link = re.sub(r'&amp;', '&', link)
+            title = m[1]
+            title = re.sub(r'&amp;', '&', title)
+            resultList.append((link, title))
+
+    for (link, title) in resultList[-numOfRecentFeeds:]:
+        print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    main()
