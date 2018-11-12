530b5c6f7db2412210f859e5f205017f7e326a22
Youngil Cho <terzeron@gmail.com>
2018-11-12 18:41:05 +0900

    re-enable a feed

--- naver/todaymusic/capture_item_link_title.py
+++ naver/todaymusic/capture_item_link_title.py
@@ -9,30 +9,19 @@ from feedmakerutil import IO
 def main():
     link = ""
     title = ""
-    state = 0
     resultList = []
     urlPrefix = "http://music.naver.com"
     
     for line in IO.read_stdin_as_line_list():
-        if state == 0:
-            m = re.search(r'<a\s+class="[^"]+"\s+href=\"(?P<url>/todayMusic/index\.nhn[^\"]+)\"[^>]*>', line)
-            if m:
-                link = urlPrefix + m.group("url")
-                link = re.sub(r'&amp;', '&', link)
-                state = 1
-        elif state == 1:
-            m = re.search(r'<span\s+class="[^"]+"\s+title=\"[^\"]+\"[^>]*>(?P<title>[^<]+)</span>', line)
-            if m:
-                title = m.group("title")
-                resultList.append((link, title))
-                state = 0
+        m = re.search(r'<a href="(?P<url>/todayMusic/index\.nhn[^\"]+)"[^>]*>(?P<title>.+)</a>', line)
+        if m:
+            link = urlPrefix + m.group("url")
+            link = re.sub(r'&amp;', '&', link)
+            title = m.group("title")
+            resultList.append((link, title))
                         
-    count = 0
     for link, title in resultList:
         print("%s\t%s" % (link, title))
-        count += 1
-        if count >= 7:
-            break
             
 
 if __name__ == "__main__":
