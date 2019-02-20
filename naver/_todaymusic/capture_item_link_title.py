5a87cb5adf0b0f66864ea3f1561ec5f5f33dd639
Youngil Cho <terzeron@gmail.com>
2019-02-20 16:49:45 +0900

    disable some feeds

---
+++ naver/_todaymusic/capture_item_link_title.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+
+import sys
+import re
+from feedmakerutil import IO
+
+
+def main():
+    link = ""
+    title = ""
+    resultList = []
+    urlPrefix = "http://music.naver.com"
+    
+    for line in IO.read_stdin_as_line_list():
+        m = re.search(r'<a href="(?P<url>/todayMusic/index\.nhn[^\"]+)"[^>]*>(?P<title>.+)</a>', line)
+        if m:
+            link = urlPrefix + m.group("url")
+            link = re.sub(r'&amp;', '&', link)
+            title = m.group("title")
+            resultList.append((link, title))
+                        
+    for link, title in resultList:
+        print("%s\t%s" % (link, title))
+            
+
+if __name__ == "__main__":
+    sys.exit(main())
