c5f0878dde7b5ade61aa1632aed39faafde91c60
Youngil Cho <terzeron@gmail.com>
2019-02-25 11:21:47 +0900

    fix a bug in handling <canvas> element with enhanced crawler script

--- mangashowme/post_process_mangashowme.py
+++ mangashowme/post_process_mangashowme.py
@@ -9,12 +9,9 @@ from feedmakerutil import IO
 
 def main():
     for line in IO.read_stdin_as_line_list():
-        m = re.search(r'^var img_list = (?P<img_list_str>\[[^\]]+\]);', line)
-        if m:
-            img_list_str = m.group("img_list_str")
-            img_list = json.loads(img_list_str)
-            for img in img_list:
-                print("<img src='%s' width='100%%'/>" % (img))
+        matches = re.findall(r'(<img[^>]*src=(?:["\'])[^\"\']+(?:["\'])[^>]*>)', line)
+        for m in matches:
+            print(m)
 
 
 if __name__ == "__main__":
