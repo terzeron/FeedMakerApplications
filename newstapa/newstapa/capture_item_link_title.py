797a7cc47f5bd5b3c57e66175142fd2e42f84550
Youngil Cho <terzeron@gmail.com>
2019-10-30 22:46:19 +0900

    modify a logic of getting article sequence from random id to date

--- newstapa/newstapa/capture_item_link_title.py
+++ newstapa/newstapa/capture_item_link_title.py
@@ -10,26 +10,35 @@ from feed_maker_util import IO
 
 def main():
     link_prefix = "https://newstapa.org"
-    
+
     num_of_recent_feeds = 10
     optlist, args = getopt.getopt(sys.argv[1:], "n:")
     for o, a in optlist:
         if o == '-n':
             num_of_recent_feeds = int(a)
-    
+
     line_list = IO.read_stdin_as_line_list()
     result_list = []
     for line in line_list:
-        matches = re.findall(r'<a href="/article/[^"]+"><img[^>]*src="https://[^"]*?/(?P<year>\d+)/(?P<month>\d+)/(?P<id>..)[^"]*"/>.*?<a href="(?P<link>/article/[^"]+)">(?P<title>[^<]+)</a>', line)
+        matches = re.findall(r'''
+        <h\d\s+class="[^"]+">
+        <a\s+href="(?P<link>/article/[^"]+)">
+        (?P<title>[^<]+)
+        </a>
+        </h\d>
+        <p\s+class="[^"]+ date">
+        (?P<date>\d+\.\d+\.\d+)
+        </p>
+        ''', line, re.VERBOSE)
         for match in matches:
-            article_id = "%s%s%03d" % (match[0], match[1], int(match[2], 16))
-            link = link_prefix + match[3]
-            title = match[4]
-            result_list.append((link, title, article_id))
+            link = link_prefix + match[0]
+            title = match[1]
+            article_date = match[2]
+            result_list.append((link, title, article_date))
 
-    for (link, title, article_id) in result_list[:num_of_recent_feeds]:
-        print("%s\t%s. %s" % (link, article_id, title))
+    for (link, title, article_date) in result_list[:num_of_recent_feeds]:
+        print("%s\t%s %s" % (link, article_date, title))
 
-        
+
 if __name__ == "__main__":
     sys.exit(main())
