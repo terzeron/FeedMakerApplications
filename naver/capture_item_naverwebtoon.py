5bd751fcf2430619ed67ff634e07206c241c0ea9
Youngil Cho <terzeron@gmail.com>
2019-12-29 18:30:14 +0900

    add a functionality to restrict the number of recent feeds

--- naver/capture_item_naverwebtoon.py
+++ naver/capture_item_naverwebtoon.py
@@ -4,6 +4,7 @@
 import sys
 import re
 from feed_maker_util import IO
+import getopt
 
 
 def main():
@@ -11,7 +12,16 @@ def main():
     title = ""
     url_prefix = "http://comic.naver.com/"
 
-    for line in IO.read_stdin_as_line_list():
+    num_of_recent_feeds = 1000
+    count = 0
+    optlist, args = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    line_list = IO.read_stdin_as_line_list()
+    result_list = []
+    for line in line_list:
         m = re.search(r'''
         <a
         \s+
@@ -43,8 +53,11 @@ def main():
             url = re.sub(r"&week(day)?=\w\w\w", "", url)
             link = url_prefix + url
             title = "%04d. %s" % (int(m.group("title1")), m.group("title2"))
-            print("%s\t%s" % (link, title))
+            result_list.append((link, title))
 
+    for (link, title) in result_list[-num_of_recent_feeds:]:
+        print("%s\t%s" % (link, title))
+            
 
 if __name__ == "__main__":
     sys.exit(main())
