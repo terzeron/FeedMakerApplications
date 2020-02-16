db4b1f8c23b72977085dbe0a753c744eb5fe6d1c
Youngil Cho <terzeron@gmail.com>
2020-02-16 22:47:13 +0900

    correct 'render-js' option in site check scripts

--- manamoa/check_manamoa.sh
+++ manamoa/check_manamoa.sh
@@ -11,7 +11,7 @@ eval "$(pyenv virtualenv-init -)"
 
 cd $FM_HOME
 . $FM_PATH/bin/setup.sh
-size=$(python3 $FM_PATH/bin/crawler.py --render-js --sleep 5 --retry 3 https://manamoa${number}.net | wc -c)
+size=$(python3 $FM_PATH/bin/crawler.py --render-js=true --sleep=5 --retry=3 https://manamoa${number}.net | wc -c)
 [ "$size" -gt 10240 ] || \
     (send_msg_to_line.sh "no service from https://manamoa${number}.net"; \
     send_msg_to_line.sh "would you check the new site? https://manamoa$((number+1)).net")
--- wfwf/check_wfwf.sh
+++ wfwf/check_wfwf.sh
@@ -12,7 +12,7 @@ eval "$(pyenv virtualenv-init -)"
 cd $FM_HOME
 . $FM_PATH/bin/setup.sh
 
-size=$(python3 $FM_PATH/bin/crawler.py --render-js --sleep 5 --retry 3 https://wfwf${number}.com | wc -c)
+size=$(python3 $FM_PATH/bin/crawler.py --render-js=true --sleep=5 --retry=3 https://wfwf${number}.com | wc -c)
 [ "$size" -gt 10240 ] || \
     (send_msg_to_line.sh "no service from https://wfwf${number}.com"; \
      send_msg_to_line.sh "would you check the new site? https://wfwf$((number+1)).com")
