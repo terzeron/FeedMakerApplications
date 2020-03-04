6b566a84f167ae9da20770bbded67f4742251072
Youngil Cho <terzeron@gmail.com>
2020-03-04 10:13:02 +0900

    modify capturing pattern

--- computer/kotlin/capture_item_link_title.py
+++ computer/kotlin/capture_item_link_title.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
 
-import io
-import os
 import sys
 import re
 from feed_maker_util import IO
@@ -13,14 +11,21 @@ def main():
     num_of_recent_feeds = 1000
     url_prefix = "https://kotlinlang.org"
     result_list = []
-    
+
     for line in IO.read_stdin_as_line_list():
         if state == 0:
-            m = re.search(r'<a class="tree-item-title[^"]*" href="(?P<link>[^"]+)">', line)
+            m = re.search(r'<a href="(?P<link>[^"]+)"\s*$', line)
             if m:
                 link = url_prefix + m.group("link")
                 state = 1
         elif state == 1:
+            m = re.search(r'^\s*class="tree-item-title', line)
+            if m:
+                state = 2
+            else:
+                # reset
+                state = 0
+        elif state == 2:
             m = re.search(r'<span class="text">(?P<title>.+)</span>', line)
             if m:
                 title = m.group("title")
@@ -32,6 +37,6 @@ def main():
         print("%s\t%03d %s" % (link, num, title))
         num = num + 1
 
-            
+
 if __name__ == "__main__":
     sys.exit(main())
