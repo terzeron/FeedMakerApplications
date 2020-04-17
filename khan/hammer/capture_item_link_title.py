caca14aa89534a09056d219c3337408b82a2d961
Youngil Cho <terzeron@gmail.com>
2020-04-17 16:19:39 +0900

    modify configuration & capturing pattern

--- khan/hammer/capture_item_link_title.py
+++ khan/hammer/capture_item_link_title.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 
-import os
 import sys
 import re
 import getopt
@@ -10,17 +9,17 @@ from feed_maker_util import IO
 
 def main():
     link_prefix = "http://news.khan.co.kr/kh_cartoon/"
-    
+
     num_of_recent_feeds = 10
-    optlist, args = getopt.getopt(sys.argv[1:], "n:")
+    optlist, _ = getopt.getopt(sys.argv[1:], "n:")
     for o, a in optlist:
         if o == '-n':
             num_of_recent_feeds = int(a)
-    
+
     line_list = IO.read_stdin_as_line_list()
     result_list = []
     for line in line_list:
-        m1 = re.search(r'<a href="\./(?P<link>khan_index\.html\?artid=\d+)[^"]*">(?P<title>[^<]+)</a>', line)
+        m1 = re.search(r'<a href=["\']\./(?P<link>khan_index\.html\?artid=\d+)[^"\']*["\']>(?P<title>[^<]+)</a>', line)
         if m1:
             link = link_prefix + m1.group("link")
             title = m1.group("title")
@@ -28,6 +27,7 @@ def main():
 
     for (link, title) in result_list[:num_of_recent_feeds]:
         print("%s\t%s" % (link, title))
+
 
 if __name__ == "__main__":
     sys.exit(main())
