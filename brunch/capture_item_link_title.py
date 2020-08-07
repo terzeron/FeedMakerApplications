1234197faca48f59472e5cb148dd6d88a26ea0b8
Youngil Cho <terzeron@gmail.com>
2020-08-07 12:41:00 +0900

    add a new feed and a new feed group

---
+++ brunch/capture_item_link_title.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+import sys
+import re
+import json
+import getopt
+import feed_maker_util
+from feed_maker_util import IO
+from crawler import Method, Crawler
+
+
+def main():
+    link = ""
+    title = ""
+    url_prefix = "https://brunch.co.kr/@"
+    state = 0
+
+    num_of_recent_feeds = 1000
+    optlist, _ = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    result_list = []
+    content = IO.read_stdin()
+    json_data = json.loads(content)
+    if json_data:
+        if "data" in json_data:
+            if "list" in json_data["data"]:
+                for item in json_data["data"]["list"]:
+                    link = url_prefix + item["user"]["profileId"] + "/" + str(item["article"]["no"])
+                    title = item["article"]["title"]
+                    result_list.append((link, title))
+
+
+    for (link, title) in result_list[:num_of_recent_feeds]:
+        print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
