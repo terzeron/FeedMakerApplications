a2131b216c2af1f91ad9a24a3523d5d7c29b1515
Youngil Cho <terzeron@gmail.com>
2021-02-23 20:20:13 +0900

    add a new feed group and a feed

---
+++ hankyung/hankyung.consensus/capture_item_link_title.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+
+import sys
+import re
+import getopt
+from feed_maker_util import IO
+
+
+def main():
+    link = ""
+    url_prefix = "http://consensus.hankyung.com"
+    title = ""
+    state = 0
+
+    num_of_recent_feeds = 3000
+    optlist, _ = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    result_list = []
+    for line in IO.read_stdin_as_line_list():
+        if state == 0:
+            m = re.search(r'<div id="content_\d+" class="[^"]+">', line)
+            if m:
+                state = 1
+        elif state == 1:
+            m = re.search(r'<strong>(?P<title>.+)</strong>', line)
+            if m:
+                title = m.group("title")
+                state = 2
+        elif state == 2:
+            m = re.search(r'<li>(?P<subtitle>.+?)</li>', line)
+            if m:
+                subtitle = m.group("subtitle")
+                if subtitle not in title:
+                    title = title + " - " + subtitle
+                state = 5
+        elif state == 5:
+            m = re.search(r'<a href="(?P<link>[^"]+)"[^>]*><img src="/images/btn_attached.gif"', line)
+            if m:
+                link = url_prefix + m.group("link")
+                if not re.search(r'(\(\d\d\d\d\d\d\)|Daily|Today|오늘|Overnight|Start with IBKS|Global Market Insight|Morning|Oneday|Eugene|Volatility)', title, re.IGNORECASE):
+                    result_list.append((link, title))
+                state = 0
+
+    for (link, title) in result_list[:num_of_recent_feeds]:
+        print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
