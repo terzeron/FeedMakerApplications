272560a4ed796aa529a53ea99798a2df62c98721
Youngil Cho <terzeron@gmail.com>
2021-06-04 09:11:09 +0900

    add some new feeds

---
+++ stibee/delta_worlding/capture_item_link_title.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+import sys
+import re
+import getopt
+import json
+from bs4 import BeautifulSoup
+from feed_maker_util import IO
+
+    
+def main():
+    link = ""
+    title = ""
+
+    num_of_recent_feeds = 30
+    optlist, _ = getopt.getopt(sys.argv[1:], "n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    result_list = []
+    #html_str = IO.read_stdin()
+    #soup = BeautifulSoup(html_str, "html.parser")
+    #json_data = soup.pre.text
+    #data = json.loads(json_data)
+    data = json.loads(IO.read_stdin())
+    if data:
+        for item in data:
+            if "subject" in item and "permanentLink" in item:
+                link = item["permanentLink"]
+                title = item["subject"]
+                result_list.append((link, title))
+
+    num = 1
+    for (link, title) in result_list[:num_of_recent_feeds]:
+        print("%s\t%003d. %s" % (link, num, title))
+        num = num + 1
+                
+            
+if __name__ == "__main__":
+    sys.exit(main())
