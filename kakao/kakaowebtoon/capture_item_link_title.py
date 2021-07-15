09310e53853e76e0abe309359598653b4ae47a86
Youngil Cho <terzeron@gmail.com>
2021-07-15 13:48:04 +0900

    modify capturing script

--- kakao/kakaowebtoon/capture_item_link_title.py
+++ kakao/kakaowebtoon/capture_item_link_title.py
@@ -13,14 +13,19 @@ def main():
 
     json_content = IO.read_stdin()
     data = json.loads(json_content)
-    if "list" in data:
-        for item in data["list"]:
-            if item["age_grade"] == 0:
-                link = url_prefix + str(item["series_id"])
-                title = item["title"] 
-                if item["waitfree"] == "Y":
-                    continue
-                print("%s\t%s" % (link, title))
+    if "section_containers" in data:
+        for container in data["section_containers"]:
+            if "section_series" in container:
+                for section in container["section_series"]:
+                    if "list" in section:
+                        data_list = section["list"]
+                        for item in data_list:
+                            if item["age_grade"] == 0:
+                                link = url_prefix + str(item["series_id"])
+                                title = item["title"] 
+                                if item["waitfree"] == "Y":
+                                    continue
+                                print("%s\t%s" % (link, title))
 
 
 if __name__ == "__main__":
