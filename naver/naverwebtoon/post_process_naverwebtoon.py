1270577a0afd26dc9d2e470e5749c16e75e8cf35
Youngil Cho <terzeron@gmail.com>
2021-07-19 15:47:11 +0900

    add image tag separately

--- naver/naverwebtoon/post_process_naverwebtoon.py
+++ naver/naverwebtoon/post_process_naverwebtoon.py
@@ -3,10 +3,11 @@
 
 import sys
 import os
+import re
 import logging
 import logging.config
 from typing import List
-from feed_maker_util import IO, exec_cmd
+from feed_maker_util import IO, exec_cmd, Config, URL
 
 
 logging.config.fileConfig(os.environ["FEED_MAKER_HOME_DIR"] + "/bin/logging.conf")
@@ -19,8 +20,20 @@ def main() -> int:
     for line in line_list:
         for exclude_keyword in exclude_keywords:
             if exclude_keyword in line:
-                print("<div>문장: %s</div>\n" % line)
-                print("<div>제외어: %s</div>\n" % exclude_keyword)
+                #print("<div>문장: %s</div>\n" % line)
+                print("<p>제외어: %s</p>\n" % exclude_keyword)
+                config = Config()
+                if not config:
+                    LOGGER.error("can't read configuration")
+                    return -1
+                rss_conf = config.get_rss_configs()
+                link = rss_conf["rss_link"]
+                m = re.search(r'\/(?P<rss_file_name>\S+\.xml)', link)
+                if m:
+                    rss_file_name = m.group("rss_file_name")
+                url = sys.argv[1]
+                md5_name = URL.get_short_md5_name(URL.get_url_path(url))
+                print("<img src='https://terzeron.com/img/1x1.jpg?feed=%s&item=%s'/>" % (rss_file_name, md5_name))
                 return 0
 
     for line in line_list:
