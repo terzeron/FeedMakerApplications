a479bdb631f4fabc6f50fd73f17110bbcab43f65
Youngil Cho <terzeron@gmail.com>
2021-08-20 10:29:37 +0900

    fix a bug in capture script from medium modify configuration of some feeds in medium

--- medium/capture_item_medium.py
+++ medium/capture_item_medium.py
@@ -1,46 +1,49 @@
 #!/usr/bin/env python
 
 
-import io
-import os
 import sys
 import re
 import getopt
 import json
-from feed_maker_util import IO
+from feed_maker_util import IO, URL
 
 
 def main():
     link = ""
     title = ""
-    url_prefix = "https://medium.com/coupang-tech/"
     state = 0
-
+    
     num_of_recent_feeds = 1000
     optlist, args = getopt.getopt(sys.argv[1:], "n:")
     for o, a in optlist:
         if o == '-n':
             num_of_recent_feeds = int(a)
 
-    content = IO.read_stdin()
+    line_list = IO.read_stdin_as_line_list()
     result_list = []
-    m = re.search(r'<script>//\s*<\!\[CDATA\[\s*window\["obvInit"\]\((?P<script>.*)\)\s*//\s*\]\]></script>', content)
-    if m:
-        json_content = m.group("script")
-        json_content = json_content.replace(r'\x3e', '\x3e') 
-        json_data = json.loads(json_content)
-        if json_data:
-            if "references" in json_data:
-                if "Post" in json_data["references"]:
-                    for item in json_data["references"]["Post"]:
-                        #import pprint
-                        #pprint.pprint(json_data["references"]["Post"][item])
-                        item_data = json_data["references"]["Post"][item]
-                        if "title" in item_data and "uniqueSlug" in item_data:
-                            if "content" in item_data and "subtitle" in item_data["content"]:
-                                title = item_data["title"] + " " + item_data["content"]["subtitle"]
-                                link = url_prefix + item_data["uniqueSlug"]
-                                result_list.append((link, title))
+    for line in line_list:
+        if state == 0:
+            m = re.search(r'<link rel="canonical" href="(?P<page_url>[^"]+)">', line)
+            if m:
+                page_url = m.group("page_url")
+                state = 1
+        elif state == 1:
+            m = re.search(r'^window\["obvInit"\]\((?P<json>.*)\)$', line)
+            if m:
+                json_content = m.group("json")
+                json_content = json_content.replace(r'\x3c', '<')
+                json_content = json_content.replace(r'\x3e', '>')
+                json_data = json.loads(json_content)
+                if json_data:
+                    if "references" in json_data:
+                        if "Post" in json_data["references"]:
+                            for item in json_data["references"]["Post"]:
+                                item_data = json_data["references"]["Post"][item]
+                                if "title" in item_data and "uniqueSlug" in item_data:
+                                    if "content" in item_data and "subtitle" in item_data["content"]:
+                                        title = item_data["title"] + " " + item_data["content"]["subtitle"]
+                                        link = page_url + "/" + item_data["uniqueSlug"]
+                                        result_list.append((link, title))
 
     for (link, title) in result_list[:num_of_recent_feeds]:
         print("%s\t%s" % (link, title))
