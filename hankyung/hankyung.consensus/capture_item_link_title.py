60ce3e595def62de482d99479cef65c3e6fa730b
Youngil Cho <terzeron@gmail.com>
2022-05-16 16:13:29 +0900

    modify configuration and capturing script of a feed

--- hankyung/hankyung.consensus/capture_item_link_title.py
+++ hankyung/hankyung.consensus/capture_item_link_title.py
@@ -4,14 +4,13 @@
 import sys
 import re
 import getopt
+import json
 from feed_maker_util import IO
 
 
 def main():
     link = ""
-    url_prefix = "http://consensus.hankyung.com"
     title = ""
-    state = 0
 
     num_of_recent_feeds = 3000
     optlist, _ = getopt.getopt(sys.argv[1:], "f:n:")
@@ -20,23 +19,21 @@ def main():
             num_of_recent_feeds = int(a)
 
     result_list = []
-    for line in IO.read_stdin_as_line_list():
-        if state == 0:
-            m = re.search(r'<td class="first txt_number">', line)
-            if m:
-                state = 1
-        elif state == 1:
-            m = re.search(r'<strong>\s*(?P<title>.+)\s*</strong>', line)
-            if m:
-                title = m.group("title")
-                state = 2
-        elif state == 2:
-            m = re.search(r'<a href="(?P<link>[^"]+)"[^>]*><img src="/images/btn_attached.gif"', line)
-            if m:
-                link = url_prefix + m.group("link")
-                if re.search(r'(주간|week(ly)?|월간?|month(ly)?|연간|년|year(ly)?)', title, re.IGNORECASE):
-                    result_list.append((link, title))
-                state = 0
+    data = IO.read_stdin()
+    json_data = json.loads(data)
+    if json_data:
+        if "data" in json_data:
+            data = json_data["data"]
+            for _, v in data.items():
+                #import pprint
+                #pprint.pprint(v)
+                if "REPORT_TITLE" in v and "REPORT_FILEPATH" in v:
+                    title = v["REPORT_TITLE"]
+                    if re.search(r'^\S+\(\d+\)\s+', title):
+                        continue
+                    if re.search(r'(주간|week(ly)?|월간?|month(ly)?|연간|년|year(ly)?)', title, re.IGNORECASE):
+                        link = v["REPORT_FILEPATH"]
+                        result_list.append((link, title))
 
     result_list = sorted(result_list, key = lambda x: x[1])
     for (link, title) in result_list[:num_of_recent_feeds]:
