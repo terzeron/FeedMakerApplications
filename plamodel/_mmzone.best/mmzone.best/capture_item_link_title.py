a0542c6e7a49257c07af9870ff8644bfc88cd080
Youngil Cho <terzeron@gmail.com>
2022-08-12 21:32:37 +0900

    recover configurations and capturing scripts from removed feeds

---
+++ plamodel/_mmzone.best/mmzone.best/capture_item_link_title.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+
+import os
+import sys
+import re
+import getopt
+from feed_maker_util import IO
+
+def main():
+    state = 0
+    url_prefix_for_forum = "http://mmzone.co.kr/mms_tool/"
+    url_prefix_for_album = "http://mmzone.co.kr/album/"
+    num_of_recent_feeds = 1000
+    optlist, args = getopt.getopt(sys.argv[1:], "f:n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    line_list = IO.read_stdin_as_line_list()
+    result_list = []
+    for line in line_list:
+        if state == 0:
+            m = re.search(r'<div id="md_text_title" onclick="location.href=\'(?P<link>[^"]+no=\d+)&[^"]+\'"[^>]*>(?P<title>.+)</div>', line)
+            if m:
+                if m.group("link").startswith("javascript"):
+                    continue
+                link = url_prefix_for_forum + m.group("link")
+                link = re.sub(r'&amp;', '&', link)
+                title = m.group("title")
+                result_list.append((link, title))
+
+            m = re.search(r'<a href="(?P<link>[^"]+no=\d+)&[^"]+">(?P<title>.+)</a>', line)
+            if m:
+                if m.group("link").startswith("javascript"):
+                    continue
+                link = url_prefix_for_forum + m.group("link")
+                link = re.sub(r'&amp;', '&', link)
+                title = m.group("title")
+                result_list.append((link, title))
+
+            m = re.search(r'<div id="scm_thumb_frame".*onclick="javascript:location.href=\'(?P<link>[^\']+)\'"', line)
+            if m:
+                link = url_prefix_for_album + m.group("link")
+                link = re.sub(r'&amp;', '&', link)
+                state = 1
+
+        elif state == 1:
+            m = re.search(r'<div id="scm_info_title">(?P<title>.+)</div>', line)
+            if m:
+                title = m.group("title")
+                result_list.append((link, title))
+                state = 0
+
+    for (link, title) in result_list[:num_of_recent_feeds]:
+        print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ plamodel/_ruliweb.plamodel/ruliweb.plamodel/capture_item_link_title.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+
+import sys
+import re
+import getopt
+from feed_maker_util import IO
+
+
+def main():
+    num_of_recent_feeds = 1000
+    optlist, _ = getopt.getopt(sys.argv[1:], "f:n:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
+
+    state = 0
+    line_list = IO.read_stdin_as_line_list()
+    result_list = []
+    for line in line_list:
+        if state == 0:
+            m = re.search(r'class="subject"', line)
+            if m:
+                state = 1
+        elif state == 1:
+            m = re.search(r'<a class="deco" href="(?P<link>[^\?"]+)[^"]*">\s*(?:<strong>)?\s*(?P<title>\S.+\S)\s*(?:</strong>)?\s*</a>', line)
+            if m:
+                link = m.group("link")
+                link = re.sub(r'&amp;', '&', link)
+                title = m.group("title")
+                state = 2
+        elif state == 2:
+            m = re.search(r'class="writer', line)
+            if m:
+                state = 3
+        elif state == 3:
+            m = re.search(r'<a[^>]*>(?P<author>.+)</a>', line)
+            if m:
+                author = m.group("author")
+                title = title + " | " + author
+                result_list.append((link, title))
+                state = 0
+
+    for (link, title) in result_list[:num_of_recent_feeds]:
+        print("%s\t%s" % (link, title))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
