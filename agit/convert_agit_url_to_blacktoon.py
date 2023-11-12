71892ee0d1ddc8dddd48ffe44c0be54118c87d31
Youngil Cho <terzeron@gmail.com>
2023-11-13 08:40:27 +0900

    update configuration and utility script for 'agit' feed group

--- agit/convert_agit_url_to_blacktoon.py
+++ agit/convert_agit_url_to_blacktoon.py
@@ -8,7 +8,7 @@ from bin.feed_maker_util import IO
 
 def main():
     for line in IO.read_stdin_as_line_list():
-        line = re.sub(r'https://agit\d+.com/azi_toons/\d+/', 'https://image.blackmanaimg.com/', line)
+        line = re.sub(r'https://agit\d+.com/azi_toons/\d+/', 'https://image.blacktoonimg.com/', line)
         if re.search(r'<img src=', line):
             line = re.sub(r' ', '%20', line)
             line = re.sub(r'<img%20src', '<img src', line)
