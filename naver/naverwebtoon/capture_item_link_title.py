8b0108ad0e94efb63078ac9e3ad3cd6b8f713922
Youngil Cho <terzeron@gmail.com>
2025-01-14 09:39:49 +0900

    modify capturing script and configuration for naverwebtoon list

--- naver/naverwebtoon/capture_item_link_title.py
+++ naver/naverwebtoon/capture_item_link_title.py
@@ -2,34 +2,37 @@
 
 
 import sys
-import re
 import json
-import pprint
+import getopt
 from bin.feed_maker_util import IO
 
 
 def main():
     link = ""
     title = ""
-    state = 0
     url_prefix = "https://comic.naver.com/webtoon/list?titleId="
     result_list = []
+
+    num_of_recent_feeds = 1000
+    optlist, args = getopt.getopt(sys.argv[1:], "n:f:")
+    for o, a in optlist:
+        if o == '-n':
+            num_of_recent_feeds = int(a)
 
     content = IO.read_stdin()
     json_data = json.loads(content)
 
-    if "titleListMap" in json_data:
-        for _, data_list in json_data["titleListMap"].items():
-            for data_item in data_list:
-                if not data_item["adult"]:
-                    if "titleId" in data_item:
-                        title_id = data_item["titleId"]
-                        link = url_prefix + str(title_id)
-                    if "titleName" in data_item:
-                        title = data_item["titleName"]
-                        result_list.append((link, title))
+    if "titleList" in json_data:
+        for data_item in json_data["titleList"]:
+            if not data_item["adult"]:
+                if "titleId" in data_item:
+                    title_id = data_item["titleId"]
+                    link = url_prefix + str(title_id)
+                if "titleName" in data_item:
+                    title = data_item["titleName"]
+                    result_list.append((link, title))
 
-    for link, title in result_list:
+    for link, title in result_list[:num_of_recent_feeds]:
         print(f"{link}\t{title}")
                     
             
