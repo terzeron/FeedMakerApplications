018b82f182c9df856db7e1e1a664e688c3104226
Youngil Cho <terzeron@gmail.com>
2025-07-14 10:32:56 +0900

    네이버 카페 피드를 위한 스크립트 수정: 작성자 이름과 제목 처리 방식 개선, 임계값 옵션 추가

--- naver/capture_item_navercafe.py
+++ naver/capture_item_navercafe.py
@@ -2,21 +2,26 @@
 
 
 import sys
+import re
 import getopt
 import json
+from math import log2
 from bin.feed_maker_util import IO
 
 
 def main():
     link = ""
     title = ""
-    url_prefix = "https://cafe.naver.com/f-e/cafes/10503958/articles/"
+    url_prefix = "https://m.cafe.naver.com/ca-fe/web/cafes/10503958/articles/"
 
     num_of_recent_feeds = 30
-    optlist, _ = getopt.getopt(sys.argv[1:], "n:f:")
+    threshold = 0.0
+    optlist, _ = getopt.getopt(sys.argv[1:], "n:f:t:")
     for o, a in optlist:
         if o == '-n':
             num_of_recent_feeds = int(a)
+        elif o == '-t':
+            threshold = float(a)
 
     result_list = []
     content = IO.read_stdin()
@@ -28,8 +33,15 @@ def main():
                 if "item" in article:
                     item = article["item"]
                     link = str(item["articleId"])
-                    title = item["writerInfo"]["nickName"].rstrip() + ": " + item["subject"].rstrip()
-                result_list.append((link, title))
+                    nickname = re.sub(r'\n', '', item["writerInfo"]["nickName"].strip())
+                    subject = re.sub(r'\n', '', item["subject"].strip())
+                    title = nickname + ": " + subject
+                    if threshold > 0.0:
+                        # (log(comment)+log(like))/log(read) + log(read)/10
+                        score = (log2(int(item["commentCount"]) + 1) + log2(int(item["likeCount"]) + 1)) / log2(int(item["readCount"]) + 1) + log2(int(item["readCount"]) + 1) / 10
+                        if score < threshold:
+                            continue
+                    result_list.append((link, title))
         
 
     for (link, title) in result_list[:num_of_recent_feeds]:
--- naver/post_process_navercafe.py
+++ naver/post_process_navercafe.py
@@ -5,7 +5,9 @@ import re
 import json
 import getopt
 import requests
-from bin.feed_maker_util import IO
+from urllib.parse import unquote
+
+from bin.feed_maker_util import IO, header_str
 
 
 def main():
@@ -28,6 +30,8 @@ def main():
             if "result" in data:
                 result = data["result"]
 
+                print(header_str)
+                
                 # 본문
                 image_list: list[str] = []
                 if "article" in result:
@@ -36,17 +40,51 @@ def main():
                     # 이미지 수집
                     if "contentElements" in article:
                         image_elements = article["contentElements"]
+                        if not image_elements:
+                            if "scrap" in article:
+                                scrap = article["scrap"]
+                                if "contentElements" in scrap:
+                                    image_elements = scrap["contentElements"]
+
                         for image_element in image_elements:
-                            image_url = image_element["json"]["image"]["url"]
-                            image_list.append(f"<img src='{image_url}' />")
+                            json_ = image_element["json"]
+                            if "image" in json_:
+                                image_url = json_["image"]["url"]
+                                if "egloos.com" in image_url or "hanafos.com" in image_url:
+                                    continue
+
+                                m = re.search(r'https://dthumb-phinf\.pstatic\.net/\?src=%22(?P<real_image_url>http.*)%22', image_url)
+                                if m:
+                                    image_url = unquote(m.group("real_image_url"))
+                                    
+                                image_list.append(f"<img src='{image_url}' />")
+                            elif "url" in json_:
+                                m = re.search(r'(?P<youtube_link>youtube.com/embed/(?P<youtube_id>\w+))', json_["url"])
+                                if m:
+                                    youtube_link = m.group("youtube_link")
+                                    youtube_id = m.group("youtube_id")
+                                    image_url = f"https://i.ytimg.com/vi_webp/{youtube_id}/maxresdefault.webp"
+                                    image_list.append(f"<a href='https://{youtube_link}'><img src='{image_url}' /></a>")
                             
                     # 본문 출력 
                     if "contentHtml" in article:
                         html = article["contentHtml"]
+                        if "scrap" in article:
+                            scrap = article["scrap"]
+                            if "contentHtml" in scrap:
+                                html += scrap["contentHtml"]
+                            
                         # 이미지 치환
                         for i in range(len(image_list)):
                             marker = f"[[[CONTENT-ELEMENT-{i}]]]"
                             html = html.replace(marker, image_list[i])
+
+                        # 클리닝
+                        html = re.sub(r' (style|class|lang)="[^"]*"', '', html)
+                        html = re.sub(r'</?span>', '', html)
+                        html = re.sub(r'<(br|img) ?/?>', r'<\1/>\n', html)
+                        html = re.sub(r'</(div|span|p|li|h\d)/?>', r'</\1>\n', html)
+                            
                         print("<div>")
                         print(html)
                         print("</div>")
@@ -54,14 +92,13 @@ def main():
                 # 댓글
                 if "comments" in result:
                     comments = result["comments"]["items"]
-                    print("<div>\n<p>--------------------- 댓글 ---------------------</p>\n<nl>")
+                    print("<div>\n<h2>--------------------- 댓글 ---------------------</h2>\n<div>\n")
                     for comment in comments:
                         nick = comment["writer"]["nick"]
                         comment_text = comment["content"]
-                        print(f"<li>{nick}: {comment_text}</li>")
-                    print("</nl>\n</div>")
-                        
+                        print(f"<p><span>{nick}:</span> <span>{comment_text}</span></p>\n")
+                    print("</div>\n</div>\n")
+
+
 if __name__ == "__main__":
     sys.exit(main())
-
-    
