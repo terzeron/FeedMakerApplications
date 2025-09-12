96a28b8282330f62de8a258cbb87a8c339998f2b
Youngil Cho <terzeron@gmail.com>
2025-09-12 15:13:31 +0900

    utils/translation.py 사용방법 변경

--- hackernoon/capture_item_hackernoon.py
+++ hackernoon/capture_item_hackernoon.py
@@ -44,7 +44,8 @@ def main():
                                         link = f"{url_prefix}/{story['slug']}"
                                         result_list.append((link, story["title"]))
 
-    result_list = Translation.translate(result_list[:num_of_recent_feeds])
+    translation = Translation()
+    result_list = translation.translate(result_list[:num_of_recent_feeds])
                                         
     for link, title in result_list[:num_of_recent_feeds]:
         print(f"{link}\t{title}")
